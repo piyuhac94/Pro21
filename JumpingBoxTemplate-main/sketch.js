ad04b1ea3757600dbd1b3fa59a106845e39d92ef
piyuhac94 <76434976+piyuhac94@users.noreply.github.com>
2021-01-01 20:24:56 +0530

    Add files via upload

---
+++ JumpingBoxTemplate-main/sketch.js
@@ -0,0 +1,33 @@
+var canvas;
+var music;
+var box1;
+var box2;
+var box3;
+var box4;
+
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+    box1 = cretesprite(200,600);
+
+
+
+
+}
+
+function draw() {
+    background("black");
+   
+
+
+
+
+
+    drawsprites();
+
+}
